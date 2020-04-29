bb2c7e61a59d18a511df4e26d65cb7c1785cb31a
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-29 18:14:29 +0200

    use a count function inside of the setTimeout method, the count increases the number by one and the function calls itself afterwards

---
+++ js/challenge.js
@@ -0,0 +1,17 @@
+// recognize event
+// callback function
+// dom functions after load
+
+// count
+function count (counter = 0){
+      counter += 1
+    count(counter)
+    }
+let timeout = window.setTimeout(count(), 1*1000)
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    
+});
