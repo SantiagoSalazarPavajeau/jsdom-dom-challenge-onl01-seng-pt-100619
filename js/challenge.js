c240e203c443dafb331f03c8031a29ff2eef9845
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-30 13:14:50 +0200

    add eventlisteners for buttons

--- js/challenge.js
+++ js/challenge.js
@@ -4,9 +4,14 @@
 
 // count
 // use a count function inside of the setTimeout method, the count increases the number by one and the function calls itself afterwards
-
+// setInterval function is defined in the global scope, and the function count is defined only with the add logic and seting the innertext, the seconds and html tag are defined outside the scope of the count function. the seconds are set as a 0 integer and setting seconds as value of the element creates a NAN
 let seconds = 0;
 let counter = document.getElementById('counter');
+let minus = document.getElementById('minus');
+let plus = document.getElementById('plus');
+let heart = document.getElementById('heart');
+let pause = document.getElementById('pause');
+let likes = document.getElementById('likes');
 
 
 function count(){
@@ -14,8 +19,10 @@ function count(){
     counter.innerText = seconds;
 
 }
-
-let interval = setInterval(count, 1000);
+document.addEventListener("DOMContentLoaded",(event) => {
+    setInterval(count, 1000);
+    
+} );
 
 
 
